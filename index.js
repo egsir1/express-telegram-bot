1e3f3a0e36b9da322701b5885585c7fe511d81e9
egsir1 <egsir111@gmail.com>
2024-09-20 16:36:17 +0900

    feat: develop express server & mongodb connection

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(express.json());
+
+const dev = async () => {
+  try {
+    mongoose
+      .connect(process.env.MONGO_URI)
+      .then(() => console.log("MongoDB Connected"))
+      .catch((error) => console.log(error));
+
+    app.listen(process.env.PORT || 4004, () => {
+      console.log(`Server initialized on port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+dev();
